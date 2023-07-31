b3a922b15c4e8a45a3c25ba76e4bbdddff07cbf2
Juan Ignacio Rodríguez-Vinçon (IgVinçon) <71676699+IgVincon@users.noreply.github.com>
2023-07-31 20:20:31 -0300

    chore: move code from main > extract_cuti

--- src/etl/extract_cuti.py
+++ src/etl/extract_cuti.py
@@ -1,16 +1,19 @@
 from utils.web_scraper import fetch_url, find_elements, extract_urls, scrape
 from requests.exceptions import ConnectionError, InvalidSchema, ReadTimeout
 from pyppeteer.errors import TimeoutError
+from json import dump
 
-def main():
+def extract():
     url = 'https://cuti.org.uy/eventos/'
     css_selector = '#mec_full_calendar_container_1779'
 
     try:
-        response = fetch_url(url)
+        response = fetch_url(url) # considerar caso en que la request no de 200 OK y que necesite manejar esa situación
         element = find_elements(response, css_selector)
-        urls = extract_urls(element)
+        urls = extract_urls(element) # considerar caso en que no haya eventos ese mes (o hasta el momento de ese mes) y como manejar esa situación
         data_set = scrape(urls)
+        with open('data/raw/cuti_extraction.json', 'w') as file:
+            dump(data_set, file)
     except InvalidSchema as e:
         # error for 'h**ps://www.google.com/'
         print(f'For the url "{url}" the error is: {e} \n')
@@ -27,6 +30,3 @@ def main():
         # error if timout in rendering the page
         print(f'For the url "{url}" the error is: {e} \n')
         pass
-
-if __name__ == "__main__":
-    main()
--- src/main.py
+++ src/main.py
@@ -1,32 +1,8 @@
-from utils.web_scraper import fetch_url, find_elements, extract_urls, scrape
-from requests.exceptions import ConnectionError, InvalidSchema, ReadTimeout
-from pyppeteer.errors import TimeoutError
+from etl import extract_cuti
 
 def main():
-    url = 'https://cuti.org.uy/eventos/'
-    css_selector = '#mec_full_calendar_container_1779'
-
-    try:
-        response = fetch_url(url)
-        element = find_elements(response, css_selector)
-        urls = extract_urls(element)
-        data_set = scrape(urls)
-    except InvalidSchema as e:
-        # error for 'h**ps://www.google.com/'
-        print(f'For the url "{url}" the error is: {e} \n')
-        pass
-    except ReadTimeout as e:
-        # error due to too much delay 
-        print(f'For the url "{url}" the error is: {e} \n')
-        pass
-    except ConnectionError as e:
-        # error for 'https://www.baaaadurl.com/'
-        print(f'For the url "{url}" the error is: {e} \n')
-        pass
-    except TimeoutError as e:
-        # error if timout in rendering the page
-        print(f'For the url "{url}" the error is: {e} \n')
-        pass
+    #extract_cuti.extract()
+    None #placeholder para evitar que indique error
 
 if __name__ == "__main__":
     main()
